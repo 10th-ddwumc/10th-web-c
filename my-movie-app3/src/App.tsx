e47d1517e078e16e4737fa50685dc9b0e0159531
lynu0204 <iyeonu0204@gmail.com>
2026-04-02 12:50:39 +0900

    App.tsx

--- my-movie-app3/src/App.tsx
+++ my-movie-app3/src/App.tsx
@@ -6,7 +6,6 @@ function App() {
   return (
     <Router>
       <Routes>
-        {/* :movieId 부분이 useParams로 가져올 변수가 됩니다 */}
         <Route path="/movies/:movieId" element={<MovieDetail />} />
       </Routes>
     </Router>
