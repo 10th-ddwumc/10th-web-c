c69d92cba7ce025d76e99d2798296fcf552a438b
이연우 <iyeonu0204@gmail.com>
2026-05-20 23:40:05 +0900

    [feat]: sidebar 재구성

---
+++ Week08_1/test/src/hooks/useSidebar.ts
@@ -0,0 +1,12 @@
+// src/hooks/useSidebar.ts
+import { useState } from "react";
+
+export function useSidebar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const open = () => setIsOpen(true);
+  const close = () => setIsOpen(false);
+  const toggle = () => setIsOpen((prev) => !prev);
+
+  return { isOpen, open, close, toggle };
+}
