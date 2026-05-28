00677f290d402cc2a2cd0beefaba67fffdaf8967
이연우 <iyeonu0204@gmail.com>
2026-05-28 13:13:37 +0900

    [feat]: useReducer

---
+++ Week09_0/react-app/src/App.tsx
@@ -0,0 +1,11 @@
+import UseReducerPage from './useReducer/UseReducerPage';
+import './App.css';
+import { type JSX } from 'react'; 
+
+export default function App() : JSX.Element  {
+  return (
+    <>
+    <UseReducerPage />
+    </>
+  );
+}
